c6ba5ffe84b5635b169be0e59b93dda9d41cdaab
Luis Carlos <luis.carlos@jornadatecnologia.com>
2021-09-27 01:33:48 -0300

    [features/Link] Criando componente do link

---
+++ src/ui/components/navigation/Link.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import NextLink, {LinkProps as NextLinkProps} from "next/link";
+import {Router} from "next/router";
+import {Link as MuiLink, LinkProps as MuiLinkProps} from "@material-ui/core";
+
+export interface LinkProps {
+    href: string;
+    mui?: MuiLinkProps;
+    next?: NextLinkProps;
+    Component?: React.ElementType;
+    onClick?: () => void;
+}
+
+const Link: React.FC<LinkProps> = ({children, href, mui, next, Component= MuiLink, ...props}) => {
+    const isNextEnv = Boolean(Router.router);
+
+    return isNextEnv ? (
+        <div>
+            <NextLink href="href" passHref {...next}>
+                <Component {...mui} {...props}>{children}</Component>
+            </NextLink>
+        </div>
+    ) : (
+        <Component href={href} {...mui} {...props}>{children}</Component>
+    );
+}
+
+export default Link;

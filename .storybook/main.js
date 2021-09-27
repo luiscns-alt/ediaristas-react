2e084cb6fdd4c7b1c8d29dc1243fe167f8f6758f
Luis Carlos <luis.carlos@jornadatecnologia.com>
2021-09-26 23:19:41 -0300

    [features/RoundedButton] Criando o compomente RoundedButton.

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,19 +3,17 @@ const path = require("path");
 const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: async (config) => {
     config.resolve.modules.push(toPath("src"));
 
     config.resolve.alias["@emotion/core"] = toPath(
       "node_modules/@emotion/react"
     );
-
-    config.resolve.alias["@emotion/styles"] = toPath(
-      "node_modules/@emotion/styles"
+    config.resolve.alias["@emotion/styled"] = toPath(
+      "node_modules/@emotion/styled"
     );
-
     config.resolve.alias["emotion-theming"] = toPath(
       "node_modules/@emotion/react"
     );

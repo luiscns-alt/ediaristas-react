e96115aea7dfc31f1dd6cab9951cafd4ea11e717
Luis Carlos <luiscarlosnors@gmail.com>
2021-10-22 21:28:49 -0300

    [Feature] Criando Hooks para verificação do cep.

---
+++ src/data/hooks/pages/useVerificarProfissionais.page.ts
@@ -0,0 +1,49 @@
+import { UserShortInformationInterface } from "data/@types/UserInterface";
+import { ApiService } from "data/services/ApiService";
+import { ValidationService } from "data/services/ValidationService";
+import { useState, useMemo } from "react";
+
+export default function useVerificarProfissionais() {
+    const [cep, setCep] = useState(""),
+        cepValido = useMemo(() => {
+            ValidationService.cep(cep);
+        }, [cep]),
+        [error, setError] = useState(""),
+        [buscaFeita, setBuscaFeita] = useState(false),
+        [carrengando, setCarrengando] = useState(false),
+        [diaristas, setDiaristas] = useState(
+            [] as UserShortInformationInterface[]
+        ),
+        [diaristasRestantes, setDiaristasRestantes] = useState(0);
+
+    async function buscarProfissionais(cep: string) {
+        setBuscaFeita(false);
+        setCarrengando(true);
+        setError("");
+        try {
+            const { data } = await ApiService.get<{
+                diaristas: UserShortInformationInterface[];
+                quantidade_diaristas: number;
+            }>(`/api/diaristas/localidades?cep=${cep.replace(/\D/g, "")}`);
+            setBuscaFeita(true);
+            setDiaristas(data.diaristas);
+            setDiaristasRestantes(data.quantidade_diaristas);
+            setCarrengando(false);
+        } catch (error) {
+            setError("CEP não encontrado");
+            setCarrengando(false);
+        }
+    }
+
+    return {
+        cep,
+        setCep,
+        cepValido,
+        buscarProfissionais,
+        error,
+        diaristas,
+        buscaFeita,
+        carrengando,
+        diaristasRestantes,
+    };
+}

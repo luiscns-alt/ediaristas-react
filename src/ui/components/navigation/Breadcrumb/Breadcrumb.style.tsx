e47e14760fd1817f949f43eefeba6631736fe111
Luis Carlos <luiscarlosnors@gmail.com>
2021-10-31 21:50:00 -0300

    [Feature] Breadcrumb

---
+++ src/ui/components/navigation/Breadcrumb/Breadcrumb.style.tsx
@@ -0,0 +1,48 @@
+import { styled } from "@material-ui/core/styles";
+// import {  } from '@material-ui/core';
+// import { BreadcrumbProps } from './Breadcrumb';
+
+export const BreadcrumbContainer = styled("ul")`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding: 0;
+    list-style: none;
+    color: ${({ theme }) => theme.palette.text.secondary};
+    counter-reset: breadcrumb-counter;
+    ${({ theme }) => theme.breakpoints.down("md")} {
+        font-size: 12px;
+        text-align: center;
+        margin: 0;
+        span {
+            display: none;
+        }
+    }
+    ${({ theme }) => theme.breakpoints.up("md")} {
+        gap: ${({ theme }) => theme.spacing(2)};
+        margin: ${({ theme }) => theme.spacing(2) + " " + theme.spacing()};
+    }
+`;
+
+export const BreadcrumbItem = styled("li", {
+    shouldForwardProp: (prop) => prop !== "isSelected",
+})<{ isSelected?: boolean }>`
+    ${({ theme }) => theme.breakpoints.down("md")} {
+        flex: auto;
+        padding: ${({ theme }) => theme.spacing() + " " + theme.spacing(3)};
+        background-color: ${({ theme, isSelected }) =>
+            theme.palette.grey[isSelected ? 200 : 100]};
+    }
+    ${({ theme }) => theme.breakpoints.down("sm")} {
+        padding: ${({ theme }) => theme.spacing()};
+    }
+
+    ${({ theme }) => theme.breakpoints.up("md")} {
+        font-weight: ${({ isSelected }) => (isSelected ? "bold" : "normal")};
+        counter-increment: breadcrumb-counter;
+        &::before {
+            content: counter(breadcrumb-counter);
+            margin-right: ${({ theme }) => theme.spacing()};
+        }
+    }
+`;
